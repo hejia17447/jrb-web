aca5d42314e7a78b531f4fe3e111da5bf3050085
hejia17447 <1744709356@qq.com>
2023-07-01 14:21:04 +0800

    系统路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -51,8 +51,38 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '首页', icon: 'dashboard' }
     }]
+  },
+
+  {
+    path: '/core/integral-grade',
+    component: Layout,
+    redirect: '/core/integral-grade/list',
+    name: 'coreIntegralGrade',
+    meta: { title: '积分等级管理', icon: 'el-icon-s-marketing' },
+    alwaysShow: true,
+    children: [
+      {
+        path: 'list',
+        name: 'coreIntegralGradeList',
+        component: () => import('@/views/core/integral-grade/list'),
+        meta: { title: '积分等级列表' }
+      },
+      {
+        path: 'create',
+        name: 'coreIntegralGradeCreate',
+        component: () => import('@/views/core/integral-grade/form'),
+        meta: { title: '新增积分等级' }
+      },
+      {
+        path: 'edit/:id',
+        name: 'coreIntegralGradeEdit',
+        component: () => import('@/views/core/integral-grade/form'),
+        meta: { title: '编辑积分等级' },
+        hidden: true
+      }
+    ]
   },
 
   {
