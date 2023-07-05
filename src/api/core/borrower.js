98b8fd815a00a1cb1c00a0332122cdb9d4658bde
HJ <1744709356@qq.com>
2023-07-05 22:17:11 +0800

    借款信息展示与审批功能

--- src/api/core/borrower.js
+++ src/api/core/borrower.js
@@ -5,7 +5,20 @@ export default {
         return request({
             url: `/admin/core/borrower/list/${page}/${limit}`,
             method: 'get',
-            params: {keyword}
+            params: { keyword }
+        })
+    },
+    show(id) {
+        return request({
+            url: `/admin/core/borrower/show/${id}`,
+            method: 'get'
+        })
+    },
+    approval(borrowerApproval) {
+        return request({
+            url: '/admin/core/borrower/approval',
+            method: 'post',
+            data: borrowerApproval
         })
     }
 }
